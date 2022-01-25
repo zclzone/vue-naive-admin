5a05c6ee54a95740777332d27b70cf2c7f7f58a4
zhangchuanlong <zhangchuanlong@dossen.com>
2022-01-25 16:26:51 +0800

    feat:添加插件vite-plugin-vue-setup-extend,支持直接在setup script标签定义name

--- build/vite/plugin/index.js
+++ build/vite/plugin/index.js
@@ -1,10 +1,11 @@
 import vue from '@vitejs/plugin-vue'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+import { unocss } from './unocss'
 import { configHtmlPlugin } from './html'
 import { configMockPlugin } from './mock'
-import { unocss } from './unocss'
 
 export function createVitePlugins(viteEnv, isBuild) {
-  const plugins = [vue(), unocss(), configHtmlPlugin(viteEnv, isBuild)]
+  const plugins = [vue(), VueSetupExtend(), unocss(), configHtmlPlugin(viteEnv, isBuild)]
 
   viteEnv?.VITE_APP_USE_MOCK && plugins.push(configMockPlugin(isBuild))
 
--- src/views/test-page/TestDialog.vue
+++ src/views/test-page/TestDialog.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup name="TestDialog">
 import { NButton } from 'naive-ui'
 const handleDelete = function () {
   $dialog.confirm({
