8e04bc78217cf80ec58a4be94bac003238dd2130
zhangchuanlong <zclzone@outlook.com>
2022-02-25 14:05:44 +0800

    mod: 去除unocss测试页多余内容

--- src/views/test-page/TestUnocss.vue
+++ src/views/test-page/TestUnocss.vue
@@ -11,39 +11,6 @@
       <p text-19>测试19px</p>
       <p text-20>测试20px</p>
     </div>
-    <div mt-30 class="content-box">
-      <p text-12>测试12px</p>
-      <p text-13>测试13px</p>
-      <p text-14>测试14px</p>
-      <p text-15>测试15px</p>
-      <p text-16>测试16px</p>
-      <p text-17>测试17px</p>
-      <p text-18>测试18px</p>
-      <p text-19>测试19px</p>
-      <p text-20>测试20px</p>
-    </div>
-    <div mt-30 class="content-box">
-      <p text-12>测试12px</p>
-      <p text-13>测试13px</p>
-      <p text-14>测试14px</p>
-      <p text-15>测试15px</p>
-      <p text-16>测试16px</p>
-      <p text-17>测试17px</p>
-      <p text-18>测试18px</p>
-      <p text-19>测试19px</p>
-      <p text-20>测试20px</p>
-    </div>
-    <div mt-30 class="content-box">
-      <p text-12>测试12px</p>
-      <p text-13>测试13px</p>
-      <p text-14>测试14px</p>
-      <p text-15>测试15px</p>
-      <p text-16>测试16px</p>
-      <p text-17>测试17px</p>
-      <p text-18>测试18px</p>
-      <p text-19>测试19px</p>
-      <p text-20>测试20px</p>
-    </div>
   </div>
 </template>
 
