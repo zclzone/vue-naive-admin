51a583fc1e72de9fc597b58268159fcc4fbce5ea
张传龙 <zclzone@outlook.com>
2022-03-24 20:47:05 +0800

    feat: 添加pageTitle路由守卫，支持动态修改页面title

--- src/router/guard/index.js
+++ src/router/guard/index.js
@@ -1,7 +1,9 @@
 import { createPageLoadingGuard } from './pageLoadingGuard'
+import { createPageTitleGuard } from './pageTitleGuard'
 import { createPermissionGuard } from './permissionGuard'
 
 export function setupRouterGuard(router) {
   createPageLoadingGuard(router)
   createPermissionGuard(router)
+  createPageTitleGuard(router)
 }
---
+++ src/router/guard/pageTitleGuard.js
@@ -0,0 +1,12 @@
+const baseTitle = import.meta.env.VITE_APP_TITLE
+
+export function createPageTitleGuard(router) {
+  router.afterEach((to) => {
+    const pageTitle = to.meta?.title
+    if (pageTitle) {
+      document.title = `${pageTitle} | ${baseTitle}`
+    } else {
+      document.title = baseTitle
+    }
+  })
+}
