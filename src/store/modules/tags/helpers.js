8973e3956666ae5458784a46e8ff1ca83948211f
张传龙 <zclzone@outlook.com>
2022-04-11 22:13:17 +0800

    feat: 多标签增加sessionStorage缓存

---
+++ src/store/modules/tags/helpers.js
@@ -0,0 +1,5 @@
+import { createSessionStorage } from '@/utils/cache'
+
+export const tagsSS = createSessionStorage({ prefixKey: 'tag_' })
+export const activeTag = tagsSS.get('activeTag')
+export const tags = tagsSS.get('tags')
