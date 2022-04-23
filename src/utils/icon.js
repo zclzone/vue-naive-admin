a6f86ee315a373382ef8dfab5b799a74ade6968d
张传龙 <zclzone@outlook.com>
2022-04-23 19:09:06 +0800

    feat: 封装renderIcon方法

---
+++ src/utils/icon.js
@@ -0,0 +1,6 @@
+import { h } from 'vue'
+import { NIcon } from 'naive-ui'
+
+export function renderIcon(icon, props = { size: 12 }) {
+  return () => h(NIcon, { ...props }, { default: () => h(icon) })
+}
