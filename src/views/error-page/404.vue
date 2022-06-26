e465ee50bf867ebc2bc38955e73c4c5ce276a21d
张传龙 <zclzone@outlook.com>
2022-06-26 15:39:44 +0800

    mod: use unocss rewrite the 404 page

--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="page-404">
-    <n-result status="404" description="抱歉，您访问的页面不存在。">
+  <div h-full flex>
+    <n-result m-auto status="404" description="抱歉，您访问的页面不存在。">
       <template #icon>
         <img src="@/assets/images/404.png" width="500" />
       </template>
       <template #footer>
-        <n-button strong secondary type="primary" @click="replace('/')">返回首页</n-button>
+        <n-button @click="replace('/')">返回首页</n-button>
       </template>
     </n-result>
   </div>
@@ -14,12 +14,3 @@
 <script setup>
 const { replace } = useRouter()
 </script>
-
-<style lang="scss" scoped>
-.page-404 {
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
