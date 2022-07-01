7564f115d6764262e34196e8a6c164c7791b7529
Sean Huang <huangchaof3112@gmail.com>
2022-07-01 15:07:01 +0800

    Breakpoints issues
    Solved the problem that the breakpoint is not in the source code location when debugging.

--- build/plugin/index.js
+++ build/plugin/index.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
  * * 扩展setup插件，支持在script标签中使用name属性
  * usage: <script setup name="MyComp"></script>
  */
-import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+import vueSetupExtend from 'vite-plugin-vue-setup-extend-plus'
 
 /**
  * * unocss插件，原子css
@@ -20,7 +20,7 @@ import { configMockPlugin } from './mock'
 import unplugin from './unplugin'
 
 export function createVitePlugins(viteEnv, isBuild) {
-  const plugins = [vue(), VueSetupExtend(), ...unplugin, configHtmlPlugin(viteEnv, isBuild), Unocss()]
+  const plugins = [vue(), vueSetupExtend(), ...unplugin, configHtmlPlugin(viteEnv, isBuild), Unocss()]
 
   if (viteEnv?.VITE_APP_USE_MOCK) {
     plugins.push(configMockPlugin(isBuild))
