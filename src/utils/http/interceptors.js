4df7d44bf124c2ab960f7b40d08b69c534daace3
张传龙 <zclzone@outlook.com>
2022-07-13 22:58:09 +0800

    docs: modify annotation

--- src/utils/http/interceptors.js
+++ src/utils/http/interceptors.js
@@ -16,17 +16,14 @@ export function reqResolve(config) {
 
   const token = getToken()
   if (!token) {
-    /**
-     * * 未登录或者token过期的情况下
-     * * 跳转登录页重新登录，携带当前路由及参数，登录成功会回到原来的页面
-     */
+    // * 未登录或者token过期的情况下,跳转登录页重新登录
     toLogin()
     return Promise.reject({ code: '-1', message: '未登录' })
   }
 
   /**
-   * * jwt token
-   * ! 认证方案: Bearer
+   * * 加上 token
+   * ! 认证方案: JWT Bearer
    */
   config.headers.Authorization = config.headers.Authorization || 'Bearer ' + token
 
