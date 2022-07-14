94b46d9bf6fc70e82603c12e53c471c831e15432
张传龙 <zclzone@outlook.com>
2022-07-14 16:39:24 +0800

    chore(unocss): update unocss config

--- unocss.config.js
+++ unocss.config.js
@@ -1,16 +1,26 @@
 import { defineConfig, presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
+  exclude: ['node_modules', '.git', '.github', '.husky', '.vscode', 'build', 'dist', 'mock', 'public', './stats.html'],
+  presets: [presetUno(), presetAttributify()],
   shortcuts: [
-    ['f-c-c', 'flex justify-center items-center'],
-    ['text-ellipsis', 'truncate'],
     ['wh-full', 'w-full h-full'],
+    ['f-c-c', 'flex justify-center items-center'],
+    ['flex-col', 'flex flex-col'],
+    ['absolute-lt', 'absolute left-0 top-0'],
+    ['absolute-lb', 'absolute left-0 bottom-0'],
+    ['absolute-rt', 'absolute right-0 top-0'],
+    ['absolute-rb', 'absolute right-0 bottom-0'],
+    ['absolute-center', 'absolute-lt f-c-c wh-full'],
+    ['text-ellipsis', 'truncate'],
   ],
   rules: [
     [/^bc-(.+)$/, ([, color]) => ({ 'border-color': `#${color}` })],
-    ['color-primary', { color: 'var(--primaryColor)' }],
-    ['bgc-primary', { backgroundColor: 'var(--primaryColor)' }],
     ['card-shadow', { 'box-shadow': '0 1px 2px -2px #00000029, 0 3px 6px #0000001f, 0 5px 12px 4px #00000017' }],
   ],
-  presets: [presetUno(), presetAttributify()],
+  theme: {
+    colors: {
+      primary: 'var(--primaryColor)',
+    },
+  },
 })
