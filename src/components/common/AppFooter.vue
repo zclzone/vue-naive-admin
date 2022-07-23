7ee613d8cfeb35dc65b965f5c83712be88d5d906
张传龙 <zclzone@outlook.com>
2022-07-23 22:10:01 +0800

    style: modify footer

--- src/components/common/AppFooter.vue
+++ src/components/common/AppFooter.vue
@@ -1,10 +1,13 @@
 <template>
-  <footer text-16 h-60 f-c-c flex-col color="#6a6a6a">
-    <p>Copyright © 2022 <span hover:color-primary cursor-pointer>大脸怪</span></p>
+  <footer text-14 f-c-c flex-col color="#6a6a6a">
     <p>
-      <a href="http://beian.miit.gov.cn/" hover="decoration-underline color-primary">赣ICP备2020015008号-1</a>
+      Copyright©2022
+      <a href="https://github.com/zclzone" target="__blank" hover="decoration-underline color-primary">大脸怪</a>
+    </p>
+    <p>
+      <a href="http://beian.miit.gov.cn/" target="__blank" hover="decoration-underline color-primary">
+        赣ICP备2020015008号-1
+      </a>
     </p>
   </footer>
 </template>
-
-<script setup></script>
