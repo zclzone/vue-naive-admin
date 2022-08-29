af983d16b9afab197c200db13676636331867a38
张传龙 <zclzone@outlook.com>
2022-08-29 10:08:18 +0800

    wip: commonPage

--- src/components/page/CommonPage.vue
+++ src/components/page/CommonPage.vue
@@ -1,9 +1,14 @@
 <template>
   <AppPage :show-footer="showFooter">
+    <header v-if="showHeader" px-15 mb-15 min-h-45 flex justify-between items-center>
+      <slot v-if="$slots.header" name="header" />
+      <template v-else>
+        <h2 color="#333" text-22 font-normal>{{ title || route.meta?.title }}</h2>
+        <slot name="action" />
+      </template>
+    </header>
+
     <n-card rounded-10 flex-1>
-      <slot v-if="showHeader" name="header">
-        <h2 mb-15 color="#333">{{ title || route.meta?.title }}</h2>
-      </slot>
       <slot />
     </n-card>
   </AppPage>
