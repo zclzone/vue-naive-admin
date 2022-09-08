a5a3472486df0aa13d456b91513b77036ccb29c5
张传龙 <zclzone@outlook.com>
2022-09-08 15:08:28 +0800

    refactor: setupExtend replace with defineOptions

--- build/plugin/index.js
+++ build/plugin/index.js
@@ -1,12 +1,6 @@
 import vue from '@vitejs/plugin-vue'
 
 /**
- * * 扩展setup插件，支持在script标签中使用name属性
- * usage: <script setup name="MyComp"></script>
- */
-import vueSetupExtend from 'vite-plugin-vue-setup-extend-plus'
-
-/**
  * * unocss插件，原子css
  * https://github.com/antfu/unocss
  */
@@ -22,7 +16,7 @@ import { configMockPlugin } from './mock'
 import unplugin from './unplugin'
 
 export function createVitePlugins(viteEnv, isBuild) {
-  const plugins = [vue(), vueSetupExtend(), ...unplugin, configHtmlPlugin(viteEnv, isBuild), Unocss()]
+  const plugins = [vue(), ...unplugin, configHtmlPlugin(viteEnv, isBuild), Unocss()]
 
   if (viteEnv?.VITE_USE_MOCK) {
     plugins.push(configMockPlugin(isBuild))
