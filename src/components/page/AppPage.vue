efdd89cd50a41c9ac0fb802b1d1fdb1f7741ef35
张传龙 <zclzone@outlook.com>
2022-09-26 17:40:41 +0800

    feat: back top

--- src/components/page/AppPage.vue
+++ src/components/page/AppPage.vue
@@ -3,6 +3,7 @@
     <section class="cus-scroll-y wh-full p-15 flex-col bg-[#f5f6fb]">
       <slot />
       <AppFooter v-if="showFooter" mt-15 />
+      <n-back-top :bottom="20" />
     </section>
   </transition>
 </template>
