5cef8e4a01b4a6f00ed6166d939186132c8e5b45
zclzone <zclzone@outlook.com>
2023-03-12 21:25:14 +0800

    fix: 暗黑模式细节

--- src/views/demo/base/unocss/index.vue
+++ src/views/demo/base/unocss/index.vue
@@ -13,50 +13,50 @@
     <div f-c-c flex-col mt-20 w-350>
       <div flex flex-wrap justify-around p-10 rounded-10 b-1 bc-ccc>
         <div w-50 h-50 b-1 rounded-5 f-c-c p-10 m-20>
-          <span w-6 h-6 rounded-3 bg-black></span>
+          <span w-6 h-6 rounded-3 bg-black dark:bg-white />
         </div>
         <div w-50 h-50 b-1 rounded-5 flex justify-between p-10 m-20>
-          <span w-6 h-6 rounded-3 bg-black></span>
-          <span w-6 h-6 rounded-3 bg-black self-end></span>
+          <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+          <span w-6 h-6 rounded-3 bg-black dark:bg-white self-end />
         </div>
         <div w-50 h-50 b-1 rounded-5 flex justify-between p-10 m-20>
-          <span w-6 h-6 rounded-3 bg-black></span>
-          <span w-6 h-6 rounded-3 bg-black self-center></span>
-          <span w-6 h-6 rounded-3 bg-black self-end></span>
+          <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+          <span w-6 h-6 rounded-3 bg-black dark:bg-white self-center />
+          <span w-6 h-6 rounded-3 bg-black dark:bg-white self-end />
         </div>
         <div w-50 h-50 b-1 rounded-5 flex justify-between p-10 m-20>
           <div flex-col justify-between>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
           </div>
           <div flex-col justify-between>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
           </div>
         </div>
         <div w-50 h-50 b-1 rounded-5 flex-col justify-between items-center p-10 m-20>
           <div flex w-full justify-between>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
           </div>
-          <div w-6 h-6 rounded-3 bg-black></div>
+          <div w-6 h-6 rounded-3 bg-black dark:bg-white />
           <div flex w-full justify-between>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
           </div>
         </div>
         <div w-50 h-50 b-1 rounded-5 flex-col justify-between p-10 m-20>
           <div flex w-full justify-between>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
           </div>
           <div flex w-full justify-between>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
           </div>
           <div flex w-full justify-between>
-            <span w-6 h-6 rounded-3 bg-black></span>
-            <span w-6 h-6 rounded-3 bg-black></span>
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
+            <span w-6 h-6 rounded-3 bg-black dark:bg-white />
           </div>
         </div>
       </div>
