a4dd39c5dbca9e84ae2c5b932ab95a618ee6cb99
zclzone <zclzone@outlook.com>
2023-10-13 11:06:32 +0800

    feat: 补充gitee地址

---
+++ src/layout/components/header/components/GiteeSite.vue
@@ -0,0 +1,11 @@
+<template>
+  <n-icon mr-20 size="18" style="cursor: pointer" @click="handleLinkClick">
+    <icon-simple-icons:gitee />
+  </n-icon>
+</template>
+
+<script setup>
+function handleLinkClick() {
+  window.open('https://gitee.com/isme-admin/vue-naive-admin')
+}
+</script>
--- src/layout/components/header/index.vue
+++ src/layout/components/header/index.vue
@@ -6,6 +6,7 @@
   <div ml-auto flex items-center>
     <MessageNotification />
     <ThemeMode />
+    <GiteeSite />
     <GithubSite />
     <FullScreen />
     <UserAvatar />
@@ -18,6 +19,7 @@ import MenuCollapse from './components/MenuCollapse.vue'
 import FullScreen from './components/FullScreen.vue'
 import UserAvatar from './components/UserAvatar.vue'
 import GithubSite from './components/GithubSite.vue'
+import GiteeSite from './components/GiteeSite.vue'
 import ThemeMode from './components/ThemeMode.vue'
 import MessageNotification from './components/MessageNotification.vue'
 </script>
