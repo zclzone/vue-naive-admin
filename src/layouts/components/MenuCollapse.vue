52a7792312186213af8f4c5240311e96aeb36311
xiaofuyesnew <xiaofuyesnew@qq.com>
2024-01-15 15:53:27 +0800

    fix:修复菜单 collapse 状态图标指示错误

--- src/layouts/components/MenuCollapse.vue
+++ src/layouts/components/MenuCollapse.vue
@@ -11,7 +11,7 @@
     class="f-c-c cursor-pointer rounded-4 p-6 text-22 transition-all-300 auto-bg-hover"
     @click="appStore.switchCollapsed"
   >
-    <i :class="appStore.collapsed ? 'i-line-md-menu-unfold-left' : 'i-line-md-menu-fold-left'" />
+    <i :class="appStore.collapsed ? 'i-line-md-menu-unfold-right' : 'i-line-md-menu-fold-left'" />
   </div>
 </template>
 
